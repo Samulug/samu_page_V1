a404ca88309004c0ef0a227caec32c5bcf28181b
Angel Samuel Rodriguez Parodi <samulug595@gmail.com>
2024-03-06 00:23:31 -0300

    Finalizada Tarjeta Anim

--- Samu page/src/App.jsx
+++ Samu page/src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { BarraNavegadora } from "./components/Barra Navegadora/Navegador";
+import './app.css'
+import { TarjetaDePresentacion } from "./components/Tarjeta de Presentacion/Presentacion";
+import { ReactDOM } from "react";
 
 export function App () {
     return (
+        <div className="app-back">
         <BarraNavegadora />
+        <TarjetaDePresentacion />
+        </div>
     )
 }
--- Samu page/src/components/Tarjeta de Presentacion/Presentacion.jsx
+++ Samu page/src/components/Tarjeta de Presentacion/Presentacion.jsx
@@ -1,7 +1,47 @@
-import function TarjetaDePresentacion ( ){
+import './presentacion.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faHtml5,faCss3Alt,faJs,faReact,faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+
+export function TarjetaDePresentacion () {
     return(
-        <body>
-            
-        </body>
+       <section>
+            <div className="card">
+                <div className="container">
+                        <div className="cardface front-face">
+                            <img src="https://avatars.githubusercontent.com/u/102266130?v=4" alt="" className='card-img'/>                   
+                        </div>
+                    <div className="cardface back-face">
+                        <div className='card-info'>
+                               <h2 className="card-tittle">Un poco sobre Samu:</h2>
+                                <p className="card-parraf">Programador Junior Web muy aparcionado por las tecnologias nuevas, facil adaptación, buen trabajo en equipo y con ganas de aprender cada dia mas.</p>
+                                <p className="card-tec">Orientado en Tecnologias como:</p>
+                                <div className='icons'>
+                                    <i>
+                                        <FontAwesomeIcon icon={faHtml5} />
+                                    </i>
+                                    <i>
+                                        <FontAwesomeIcon icon={faCss3Alt} />
+                                    </i>
+                                    <i>
+                                        <FontAwesomeIcon icon={faJs} />
+                                    </i>  
+                                    <i>
+                                        <FontAwesomeIcon icon={faReact} />    
+                                    </i>
+                                </div>
+                                <div className='icons'>
+                                    <a  href='https://github.com/Samulug?tab=repositories'>
+                                        <FontAwesomeIcon icon={faGithub} />
+                                    </a>
+                                    <a  href="https://www.linkedin.com/in/samuelrodriguezparodi/">
+                                        <FontAwesomeIcon icon={faLinkedin} />
+                                    </a>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
