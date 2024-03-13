036034452bd1e629918cd334c745dca48b8eb24d
Angel Samuel Rodriguez Parodi <samulug595@gmail.com>
2024-03-13 10:49:13 -0300

    cambio en API

--- Samu page/src/App.jsx
+++ Samu page/src/App.jsx
@@ -3,24 +3,37 @@ import { BarraNavegadora } from "./components/Barra Navegadora/Navegador";
 import './app.css'
 import { TarjetaDePresentacion } from "./components/Tarjeta de Presentacion/Presentacion";
 import { BloqueDeContenido } from "./components/Bloque de Contenido/BloqueDeContenido";
-import { useFetch  } from "./useFetch";
+import { useEffect , useState } from "react";
 
 export function App () {
-    const {img} = useFetch(`https://my-json-server.typicode.com/Samulug/samu_page_V1`)
+
+    const [imageUrls, setImageUrls] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch(`https://my-json-server.typicode.com/Samulug/samu_page_V1/imgs`);
+                const data = await response.json();
+                setImageUrls(data.map(item => item.img));
+            } catch (error) {
+                console.error("Error al obtener los datos:", error);
+            }
+        }
+
+        fetchData();
+    }, []);
 
     return (
         <div className="app-back">
+
             <BarraNavegadora />  
             <section>
                 <div className="contenedor-principal">
                     <TarjetaDePresentacion  />
                     <div className="contenedor-bloq">
-                        <BloqueDeContenido />
-                        <BloqueDeContenido />
-                        <BloqueDeContenido />
-                        <BloqueDeContenido />
-                        <BloqueDeContenido />
-                        <BloqueDeContenido />
+                        {imageUrls.map((imageUrl, index) => (
+                            <BloqueDeContenido key={index} imageUrl={imageUrl} />
+                        ))}
                     </div>
                 </div>
             </section>
--- Samu page/src/components/Bloque de Contenido/BloqueDeContenido.jsx
+++ Samu page/src/components/Bloque de Contenido/BloqueDeContenido.jsx
@@ -1,13 +1,14 @@
 import '../Bloque de Contenido/bloquedecontenido.css'
 
 
-export function BloqueDeContenido () {
+export function BloqueDeContenido ({imageUrl}) {
+    console.log(imageUrl)
     return(
         <>  
                 <div className='bloque'>
                     <div className='container-bloq'>
                         <div className='bloque-interior'>
-                            <img src='https://avatars.githubusercontent.com/u/102266130?v=4' alt="" className='img-bloq'/>
+                            <img src={imageUrl} alt="Imagen de Pagina" className='img-bloq'/>
                             <h1 className='tittle-bloq'>Psd sdasd sdasd sdasd sdasd agnina de Organizacion Empresaenrial</h1>
                             <p className='parraf-bloq'>Pagina para orgasd sdsad sdasd asda sdasd asdasd sdasd sda sadsdasdnizar equipos y personal en nuestra empresa</p>
                             <button className='boton-bloq'>Visitar Pagina</button>
