a255e8563b17a3138c11ed36272256bf785da8d2
Angel Samuel Rodriguez Parodi <samulug595@gmail.com>
2024-03-15 11:19:17 -0300

    Cambio en boton

--- Samu page/src/components/Bloque de Contenido/BloqueDeContenido.jsx
+++ Samu page/src/components/Bloque de Contenido/BloqueDeContenido.jsx
@@ -3,20 +3,39 @@ import '../Bloque de Contenido/bloquedecontenido.css'
 
 export function BloqueDeContenido ({imageUrl, title, text, redirec}) {
     return(
-        <>  
-                <div className='bloque'>
-                    <div className='container-bloq'>
-                        <div className='bloque-interior'>
-                            <img src={imageUrl} alt="Imagen de Pagina" className='img-bloq'/>
-                            <h1 className='tittle-bloq'>{title}</h1>
-                            <p className='parraf-bloq'>{text}</p>
-                            <a href={redirec}>
-                                <button className='boton-bloq'>Visitar Pagina</button>
-                            </a>
-                            <div className='bloque-back'></div>
+        <>  <div className='bloque-back'>
+                    <div className='bloque'>
+                        <div className='container-bloq'>
+                            <div className='bloque-interior'>
+                                <img src={imageUrl} alt="Imagen de Pagina" className='img-bloq'/>
+                                <h1 className='tittle-bloq'>{title}</h1>
+                                <p className='parraf-bloq'>{text}</p>
+                                <a href={redirec}>
+                                    <button className='boton-bloq'>
+                                    <span class="span-mother">
+                                        <span>V</span>
+                                        <span>i</span>
+                                        <span>s</span>
+                                        <span>i</span>
+                                        <span>t</span>
+                                        <span>a</span>
+                                        <span>r</span>
+                                    </span>
+                                    <span class="span-mother2"> 
+                                        <span>V</span>
+                                        <span>i</span>
+                                        <span>s</span>
+                                        <span>i</span>
+                                        <span>t</span>
+                                        <span>a</span>
+                                        <span>r</span>
+                                    </span>
+                                    </button>
+                                </a>
+                            </div>
                         </div>
                     </div>
-                </div>
+            </div>
         </>
     )
 }
