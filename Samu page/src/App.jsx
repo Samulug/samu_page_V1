d94d7032e4f4d9acde0073a1d149089523e52904
Angel Samuel Rodriguez Parodi <samulug595@gmail.com>
2024-03-16 02:30:51 -0300

    Cambio de aspecto y intento de transicion

--- Samu page/src/App.jsx
+++ Samu page/src/App.jsx
@@ -1,47 +1,61 @@
 import React from "react";
 import { BarraNavegadora } from "./components/Barra Navegadora/Navegador";
-import './app.css'
+import "./app.css";
 import { TarjetaDePresentacion } from "./components/Tarjeta de Presentacion/Presentacion";
 import { BloqueDeContenido } from "./components/Bloque de Contenido/BloqueDeContenido";
-import { useEffect , useState } from "react";
+import { useEffect, useState } from "react";
 
-export function App () {
-    const [data, setData] = useState({ imgs: [], tittles: [], info: [] , direcciones: [], });
+export function App() {
+  const [data, setData] = useState({
+    imgs: [],
+    tittles: [],
+    info: [],
+    direcciones: [],
+  });
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch(`https://my-json-server.typicode.com/Samulug/samu_page_V1/db`);
-                const jsonData = await response.json();
-                setData(jsonData);
-            } catch (error) {
-                console.error("Error al obtener los datos:", error);
-            }
-        }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          `https://my-json-server.typicode.com/Samulug/samu_page_V1/db`
+        );
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error al obtener los datos:", error);
+      }
+    }
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    return (
-        <div className="app-back">
-
-            <BarraNavegadora />  
-            <section>
-                <div className="contenedor-principal">
-                    <TarjetaDePresentacion  />
-                    <div className="contenedor-bloq">
-                        {data.imgs.map((img, index) => (
-                            <BloqueDeContenido 
-                                key={index} 
-                                imageUrl={img.img}
-                                title={data.tittles[index].title}
-                                text={data.info[index].text}
-                                redirec={data.direcciones[index].url}
-                            />
-                        ))}
-                    </div>
-                </div>
-            </section>
+  return (
+    <div className="app-back">
+      <BarraNavegadora />
+      <section>
+        <div className="contenedor-principal">
+          <TarjetaDePresentacion />
+          <h1 className="title-port">PortFolio</h1>
+          <div className="contenedor-bloq">
+            {data.imgs.map((img, index) => (
+              <BloqueDeContenido
+                key={index}
+                imageUrl={img.img}
+                title={data.tittles[index].title}
+                text={data.info[index].text}
+                redirec={data.direcciones[index].url}
+              />
+            ))}
+          </div>
         </div>
-    )
+      </section>
+      {data.imgs.length >= 3 && (
+                <button className="next-port">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"></path>
+                    </svg>
+                </button>
+            )}
+    </div>
+  );
 }
--- Samu page/src/components/Bloque de Contenido/BloqueDeContenido.jsx
+++ Samu page/src/components/Bloque de Contenido/BloqueDeContenido.jsx
@@ -1,43 +1,46 @@
-import '../Bloque de Contenido/bloquedecontenido.css'
+import "../Bloque de Contenido/bloquedecontenido.css";
 
-
-export function BloqueDeContenido ({imageUrl, title, text, redirec}) {
-    return(
-        <>  <div className='bloque-back'>
-                    <div className='bloque'>
-                        <div className='container-bloq'>
-                            <div className='bloque-interior'>
-                                <img src={imageUrl} alt="Imagen de Pagina" className='img-bloq'/>
-                                <h1 className='tittle-bloq'>{title}</h1>
-                                <p className='parraf-bloq'>{text}</p>
-                                <a href={redirec}>
-                                    <button className='boton-bloq'>
-                                    <span class="span-mother">
-                                        <span>V</span>
-                                        <span>i</span>
-                                        <span>s</span>
-                                        <span>i</span>
-                                        <span>t</span>
-                                        <span>a</span>
-                                        <span>r</span>
-                                    </span>
-                                    <span class="span-mother2"> 
-                                        <span>V</span>
-                                        <span>i</span>
-                                        <span>s</span>
-                                        <span>i</span>
-                                        <span>t</span>
-                                        <span>a</span>
-                                        <span>r</span>
-                                    </span>
-                                    </button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+export function BloqueDeContenido({ imageUrl, title, text, redirec }) {
+  return (
+    <>
+      <div className="bloque-back">
+        <div className="bloque">
+          <div className="container-bloq">
+            <div className="bloque-interior">
+              <img src={imageUrl} alt="Imagen de Pagina" className="img-bloq" />
+              <h1 className="tittle-bloq">{title}</h1>
+              <p className="parraf-bloq">{text}</p>
+              <div className="container-a">
+              <a className="a-boton-bloq" href={redirec}>
+                <button className="boton-bloq">
+                  <span class="span-mother">
+                    <span>V</span>
+                    <span>i</span>
+                    <span>s</span>
+                    <span>i</span>
+                    <span>t</span>
+                    <span>a</span>
+                    <span>r</span>
+                  </span>
+                  <span class="span-mother2">
+                    <span>V</span>
+                    <span>i</span>
+                    <span>s</span>
+                    <span>i</span>
+                    <span>t</span>
+                    <span>a</span>
+                    <span>r</span>
+                  </span>
+                </button>
+              </a>
+              </div>
             </div>
-        </>
-    )
+          </div>
+          <div className="techs"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 //Busque en mis repositorios de github:
 //nombre , descrippcion, el link y ver como hacer una preview
